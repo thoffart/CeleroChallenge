e357717af7965e34091dcb49740483e13d944a16
thoffart <thiagohoffart321920@gmail.com>
2019-02-11 09:56:43 -0200

    fix(card-heroes): now card-heroes only receive one hero from search-heroes

--- src/app/components/card-heroes/card-heroes.component.ts
+++ src/app/components/card-heroes/card-heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-card-heroes',
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 
 export class CardHeroesComponent implements OnInit {
-    @Input() heroes: any;
+    @Input() hero: any;
     constructor() { }
 
     ngOnInit() { }
--- src/app/components/search-heroes/search-heroes.component.ts
+++ src/app/components/search-heroes/search-heroes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Player } from './../../models/player.model';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-search-heroes',
@@ -9,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class SearchHeroesComponent implements OnInit {
     heroes: any = null;
     searching = false;
+    @Output() choosenHeroeEmitter: EventEmitter<any> = new EventEmitter();
     constructor() { }
 
     ngOnInit() { }
@@ -20,4 +22,5 @@ export class SearchHeroesComponent implements OnInit {
     updateSearching($searching: boolean) {
         this.searching = $searching;
     }
+
 }
